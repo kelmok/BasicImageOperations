c5d6dbad5602e7bcaa3b6d3161bbf2b145db7065
Kelvin Mok <kelmok@users.noreply.github.com>
2018-11-29 14:23:44 -0500

    Update downsampleEveryN.py

--- downsampleEveryN.py
+++ downsampleEveryN.py
@@ -1,17 +1,16 @@
 # downsampleEveryN.py
-# takes input filename and 'downsamples' image by taking every Nth column and repeating it N times
+# takes input filename and 'downsamples' image by taking every Nth column and repeati$
 # Author: kelvin.mok@mail.mcgill.ca
-
 import numpy as np
 import PIL.Image as Image
 
-class downsampleEveryN:
-
-  def __init__(self, filename_image, rate_N):
+class downsampleEveryN():
+  def __init__(self, filename_image, filename_imgout, rate_N):
     self.filename   = filename_image
-    self.image      = Image.open(filename)
-    self.imgarr     = np.asarray(img,np.int)
-    self.imgarr_ds  = imgarr(:,::rate_N)
+    self.image      = Image.open(self.filename)
+    imgarr          = np.asarray(self.image,np.int)
+    imgarr_ds       = np.copy(imgarr[:,::rate_N])
     imgarr          = np.repeat(imgarr_ds,rate_N,1)
-    imgout          = Image.fromarray(imgarr)
-    return imgout
+    imgds           = Image.fromarray(np.array(np.uint8(imgarr)))
+    imgds.save(filename_imgout,"tiff")
+
